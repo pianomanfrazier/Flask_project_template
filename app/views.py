9c0ea929f39bfc689c850fafa7d9a81e27065964
Ryan Frazier <pianomanfrazier@gmail.com>
2016-09-15 14:07:27 -0600

    add contact page, contact form and timestamp

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app, freezer
 
+from datetime import datetime
+
 @app.route('/')
 @app.route('/index/')
 def index():
@@ -9,6 +11,14 @@ def index():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+@app.context_processor
+def get_date():
+    return dict(time=datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
